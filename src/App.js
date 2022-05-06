1c0e45f52e519af123d8abf49d724c87883120b5
nicolskiprop <yegonicholas@gmail.com>
2022-05-06 13:45:58 +0300

    dash

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header className='header'>
+        <span className='text'>
+          <h1 className='h1'>Gif portal</h1>
+          <p className='p1'>View your GIF collection in the metaverse</p>
+        </span>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;

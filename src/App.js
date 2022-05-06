9fc17d072103ee4c568dee88b60694507905ed0e
nicolskiprop <yegonicholas@gmail.com>
2022-05-06 19:08:23 +0300

    connect wallet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('phantom wallet found!');
+        }
+      } else {
+        alert('Solana object not found! Get a phantom wallet 👻')
+      }
+    } catch (error) {
+      console.error((error));
+    }
+  };
+
+/*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+
+  }, []);
+
   return (
     <div >
       <header className='header'>

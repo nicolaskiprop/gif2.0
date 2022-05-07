c5d4e082506c55ffa97843679082c61c95a123ed
nicolskiprop <yegonicholas@gmail.com>
2022-05-07 14:37:43 +0300

    gif rendered

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Twitter from '../src/assets/Twitter.svg';
 
 
 //constants
+const TEST_GIFS = [
+  'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+]
+
+
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = 'https:/twitter.com/${TWITTER_HANDLE}';
 
@@ -55,6 +63,19 @@ const App = () => {
     </button >
   );
 
+
+
+  const renderConnectedContainer = () => (
+    <div className='connectedContainer'>
+      <div className='gifGrid'>
+        {TEST_GIFS.map(gif => (
+          <div className='gifItem' key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
   /*
      * When our component first mounts, let's check to see if we have a connected
      * Phantom Wallet
@@ -74,7 +95,7 @@ const App = () => {
       <div className={walletAddress ? 'authed-container' : 'container'}>
       <header className='header'>
         <span className='text'>
-          <h1 className='h1'>Gif portal</h1>
+          <h1 className='h1'>🖼 Gif portal</h1>
           <p className='p1'>View your GIF collection in the metaverse</p>
           {!walletAddress && renderNotConnectedContainer()}
         </span>
